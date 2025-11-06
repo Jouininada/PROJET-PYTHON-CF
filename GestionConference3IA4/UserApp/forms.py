7e196eae8ed097f494b54a586fe4cc5afd54db1b
Nada Jouini <nada.jouini@example.com>
2025-11-06 02:01:42 +0100

    06/11 commit

---
+++ GestionConference3IA4/UserApp/forms.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .models import User
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "affiliation",
+            "nationality",
+            "password1",
+            "password2",
+        ]
+        widgets = {
+            "email": forms.EmailInput(),
+        }
